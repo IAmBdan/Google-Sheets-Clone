bd05b41c14c5d89621c3f38249ab2d8ea6b62101
Chris <125088905+wyattchris@users.noreply.github.com>
2024-05-31 12:22:34 -0400

    test is working!

--- src/server/api/routers/userRouter.ts
+++ src/server/api/routers/userRouter.ts
@@ -7,7 +7,9 @@ const prisma = new PrismaClient();
 export const userRouter = createTRPCRouter({
     getAll: publicProcedure.query(async () => {
         const user = await prisma.user.findMany();  
+        console.log(user);
         const result = user.map(u => ({ user: u.id }));
+        console.log(result);
         return { success: true, message: 'Users', value: result };
     }),
 })
--- src/tests/userRoutes.test.ts
+++ src/tests/userRoutes.test.ts
@@ -1,8 +1,11 @@
-import { api } from "../trpc/server";
+import axios from "axios";
+
+const endpoint = "http://localhost:3000/api/trpc";
 
 describe('userRoutes', () => {
     test('gets all users', async () => {
-        const users = await api.user.getAll();
+        const users = await axios.get(endpoint + '/user.getAll');
+        console.log(users)
         expect(users).toBeDefined();
     });
 });
