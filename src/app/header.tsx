f9031a9a59ab00fec786322e3d724ec30a982508
Brooke Chalmers <breq@breq.dev>
2024-06-06 21:54:40 -0400

    move cell/page/grid components, update styling

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,15 +1,27 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 // import "bootstrap/dist/js/bootstrap.bundle.min";
 
 export default function Header() {
-  return <>
-  <header className="bg-gray-200 p-8">Welcome to Husksheet!</header>
-
-  <div className="btn-toolbar">
-    <div className='btn-group'>
-      <button className='btn-primary'>Log In</button>
-      <button className='btn-primary'>Sign Up</button>
-    </div>
-  </div>
-  </>
+  return (
+    <header className="bg-gray-200 p-4">
+      <div className="flex flex-row justify-between">
+        <div className="flex flex-col gap-2">
+          <span className="text-2xl">My Spreadsheet</span>
+          <div className="flex flex-row gap-2">
+            <button className="hover:underline">File</button>
+            <button className="hover:underline">Edit</button>
+            <button className="hover:underline">Help</button>
+          </div>
+        </div>
+        <div className="flex flex-row gap-2 self-start">
+          <button className="text-gray-600 hover:text-black hover:underline">
+            Log In
+          </button>
+          <button className="text-gray-600 hover:text-black hover:underline">
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </header>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,21 @@
-import { api } from "~/trpc/server";
-import axios from "axios";
+// import { api } from "~/trpc/server";
 
-import Grid from "~/grid";
+import Grid from "./grid";
 import Header from "./header";
 import InputLine from "./inputline";
-import ButtonToolbar from "./btnmenu";
-import { useEffect } from "react";
-import Dummy from "./_components/dummy";
 
 export default async function Home() {
-  const data = await api.publisher.getPublishers();
-  console.log(data);
-  const users = await api.user.getAll();
-  console.log(users);
-  const publishers = await api.publisher.register({ name: "test" });
-  console.log(publishers);
+  // const data = await api.publisher.getPublishers();
+  // console.log(data);
+  // const users = await api.user.getAll();
+  // console.log(users);
+  // const publishers = await api.publisher.register({ name: "test" });
+  // console.log(publishers);
 
   return (
     <main className="flex h-full flex-col">
       {/* <Dummy /> */}
       <Header />
-      <ButtonToolbar />
       <InputLine />
       <Grid numColumns={26} numRows={100} />
     </main>
