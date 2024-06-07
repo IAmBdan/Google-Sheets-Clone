bf308403a387ccd461f037db3051a50dfe0119ed
Brooke Chalmers <breq@breq.dev>
2024-06-06 22:44:51 -0400

    initial landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
-// import { api } from "~/trpc/server";
-
-import Sheet from "./sheet";
+const SHEETS = [
+  {
+    name: "my awesome spreadsheet",
+    author: "Author 1",
+  },
+  {
+    name: "another spreadsheet",
+    author: "Author 1",
+  },
+  {
+    name: "a third spreadsheet",
+    author: "Author 1",
+  },
+];
 
 export default async function Home() {
   // const data = await api.publisher.getPublishers();
@@ -10,7 +21,19 @@ export default async function Home() {
   // const publishers = await api.publisher.register({ name: "test" });
   // console.log(publishers);
 
-  "use client";
-
-  return <Sheet />;
+  return (
+    <div className="mx-auto my-16 flex max-w-sm flex-col gap-4">
+      <h1 className="text-center text-2xl">Spreadsheets</h1>
+      {SHEETS.map(({ name, author }) => (
+        <a
+          key={name}
+          className="flex w-full flex-col rounded border border-black p-2"
+          href={"/sheet"}
+        >
+          <h2 className="text-xl font-bold">{name}</h2>
+          <span>{author}</span>
+        </a>
+      ))}
+    </div>
+  );
 }
---
+++ src/app/sheet/page.tsx
@@ -0,0 +1,5 @@
+import SheetView from "../sheetView";
+
+export default function Sheet() {
+  return <SheetView />;
+}
