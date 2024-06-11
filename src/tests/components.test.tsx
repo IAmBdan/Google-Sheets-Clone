ada8d24f704a90266dae0f435fbecb23be521891
Brooke Chalmers <breq@breq.dev>
2024-06-11 17:53:20 -0400

    Tests for Grid and SheetView components

--- src/tests/components.test.tsx
+++ src/tests/components.test.tsx
@@ -1,9 +1,12 @@
 import { expect, describe, it } from "vitest";
-import { render } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import Cell from "../app/cell";
 import { Ref } from "../classes/ref";
 import { Sheet } from "../classes/sheets";
 import { Publisher } from "../classes/publisher";
+import Grid from "../app/grid";
+import { numberToColumnLabel } from "../utils/numberToColumnLabel";
+import SheetView from "../app/sheetView";
 
 function makeSheet(content: string | number | { formula: string } | null) {
   const sheet = new Sheet(
@@ -55,10 +58,78 @@ describe("Cell component", () => {
     );
     expect(container).toMatchSnapshot();
   });
+
+  it("updates the value when the cell changes", async () => {
+    const sheet = makeSheet(null);
+
+    render(<Cell cellRef={new Ref("$A1")} sheet={sheet} />);
+
+    act(() => {
+      sheet.setCell(new Ref("$A1"), "Hello World!");
+    });
+
+    expect(screen.getByDisplayValue("Hello World!")).toBeDefined();
+  });
 });
 
-// const component = renderer.create(
-//   <Link page="http://www.facebook.com">Facebook</Link>,
-// );
-// let tree = component.toJSON();
-// expect(tree).toMatchSnapshot();
+describe("Grid component", () => {
+  it("renders correctly", () => {
+    const sheet = makeSheet(null);
+
+    const { container } = render(<Grid sheet={sheet} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("renders the column headers", () => {
+    const sheet = new Sheet(
+      2,
+      100,
+      "___",
+      new Publisher("Test Publisher", 1),
+      [],
+    );
+
+    render(<Grid sheet={sheet} />);
+
+    for (let i = 1; i <= 100; i++) {
+      expect(screen.getAllByText(numberToColumnLabel(i))).toBeTruthy();
+    }
+  });
+
+  it("renders the row headers", () => {
+    const sheet = new Sheet(
+      100,
+      2,
+      "___",
+      new Publisher("Test Publisher", 1),
+      [],
+    );
+
+    render(<Grid sheet={sheet} />);
+
+    for (let i = 1; i <= 100; i++) {
+      expect(screen.getAllByText(i.toString())).toBeTruthy();
+    }
+  });
+
+  it("renders the appropriate number of cells", () => {
+    const sheet = new Sheet(
+      13,
+      17,
+      "___",
+      new Publisher("Test Publisher", 1),
+      [],
+    );
+
+    render(<Grid sheet={sheet} />);
+
+    expect(screen.getAllByRole("textbox")).toHaveLength(13 * 17);
+  });
+});
+
+describe("SheetView component", () => {
+  it("renders correctly", () => {
+    const { container } = render(<SheetView />);
+    expect(container).toMatchSnapshot();
+  });
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
+    globals: true,
     include: ["src/tests/components.test.tsx"],
     environment: "jsdom",
   },
