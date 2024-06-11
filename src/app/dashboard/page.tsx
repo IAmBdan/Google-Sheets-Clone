65dc01fec4eea7ea593c641753e7a074fa1d4b9f
Chris <125088905+wyattchris@users.noreply.github.com>
2024-06-11 15:51:50 -0400

    change sheets to be dynamic in dashboard based upon logged in user

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,23 +4,9 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-const SHEETS = [
-  {
-    name: "my awesome spreadsheet",
-    author: "Author 1",
-  },
-  {
-    name: "another spreadsheet",
-    author: "Author 1",
-  },
-  {
-    name: "a third spreadsheet",
-    author: "Author 1",
-  },
-];
-
 export default function Dashboard() {
-  const [client, setClient] = useState("");
+  // CIRCLE BACK TO THIS
+  const [client, setClient] = useState("chris");
   const [sheetName, setSheetName] = useState("");
   const [sheets, setSheets] = useState([]);
 
@@ -40,32 +26,6 @@ export default function Dashboard() {
     void fetchSheets();
   }, []);
 
-  const createSheet = async () => {
-    try {
-      const response = await axios.post("/api/v1/createSheet", {
-        publisher: client,
-        sheet: sheetName,
-      }); // Adjust the payload as needed
-      setSheets([...sheets, response.data]);
-      console.log(response.data);
-    } catch (error) {
-      console.error("Error creating sheet:", error);
-    }
-  };
-
-  const deleteSheet = async (id: number) => {
-    try {
-      await axios.post("/api/v1/deleteSheet", {
-        publisher: client,
-        sheet: sheetName,
-      }); // Adjust to your actual endpoint
-      setSheets(sheets.filter((sheet) => sheet.id !== id));
-      console.log(`Sheet with id ${id} deleted`);
-    } catch (error) {
-      console.error("Error deleting sheet:", error);
-    }
-  };
-
   return (
     <div className="mx-auto my-16 flex max-w-sm flex-col gap-4">
       <div>
@@ -78,21 +38,15 @@ export default function Dashboard() {
             placeholder="Enter client name"
             className="w-full rounded-md border p-2"
           />
-          <button
-            onClick={createSheet}
-            className="ml-4 rounded-md bg-blue-500 px-4 py-2 text-white transition hover:bg-blue-600"
-          >
-            Create New Sheet
-          </button>
         </div>
-        {SHEETS.map(({ name, author }) => (
+        {sheets.map(({ publisher, sheet }) => (
           <a
-            key={name}
+            key={publisher}
             className="flex w-full flex-col rounded border border-black p-2"
             href={"/sheet"}
           >
-            <h2 className="text-xl font-bold">{name}</h2>
-            <span>{author}</span>
+            <h2 className="text-xl font-bold">{sheet}</h2>
+            <span>{publisher}</span>
           </a>
         ))}
       </div>
