91f0ce3efa838870c30281af78d6167523acfe81
Chris <125088905+wyattchris@users.noreply.github.com>
2024-06-11 16:10:57 -0400

    sheet data

--- src/app/sheetView.tsx
+++ src/app/sheetView.tsx
@@ -6,6 +6,13 @@ import { useState } from "react";
 import { Sheet } from "../classes/sheets";
 import { Publisher } from "../classes/publisher";
 
+/*
+{
+  ["cell": "A1", "value": "hello!",
+  "cell": "A1", "value": "hello!"]
+}
+*/
+
 export default function SheetView() {
   const [sheet] = useState(() => {
     const sheet = new Sheet(
@@ -15,13 +22,13 @@ export default function SheetView() {
       new Publisher("My Publisher", 1),
       [],
     );
+    // unpack the data and set the cell values 
     sheet.setCell(new Ref("A", 1), "hello!");
     return sheet;
   });
 
   return (
     <main className="flex h-full flex-col">
-      {/* <Dummy /> */}
       <Grid sheet={sheet} />
     </main>
   );
