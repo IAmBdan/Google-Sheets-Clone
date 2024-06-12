0d02160ed2834a16148f69c2b3d36a48fcde4c75
Giastina <morano.gia@northeastern.edu>
2024-06-11 23:28:03 -0400

    update ui on dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -72,38 +72,40 @@ export default function Dashboard() {
   };
 
   return (
-    <div className="mx-auto my-16 flex max-w-sm flex-col gap-4">
-      <div>
-        <h1 className="text-center text-2xl">{client}&apos;s Spreadsheets</h1>
+    <div className="mx-auto my-16 flex max-w-sm flex-col gap-8">
+      <div className="text-center">
+        <h1 className="text-2xl">{client}&apos;s Spreadsheets</h1>
+      </div>
+      <div className="flex justify-center">
         <button
           onClick={handleCreateNewSheet}
-          className="ml-4 rounded-md bg-blue-500 px-4 py-2 text-white transition hover:bg-blue-600"
+          className="rounded-md bg-blue-500 px-4 py-2 text-white transition hover:bg-blue-600"
         >
           Create New Sheet
         </button>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-          {sheets.map(({ publisher, sheet }) => (
-            <div
-              key={publisher}
-              className="relative flex w-full flex-col rounded border border-black p-2"
+      </div>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+        {sheets.map(({ publisher, sheet }) => (
+          <div
+            key={publisher}
+            className="relative flex w-full flex-col rounded border border-black p-2"
+          >
+            <a href={`/sheet/${publisher}/${sheet}`} className="flex-grow">
+              <h2 className="text-xl font-bold">{sheet}</h2>
+              <span>{publisher}</span>
+            </a>
+            <button
+              onClick={async (e) => {
+                e.preventDefault(); // Prevent default anchor click behavior
+                await deleteSheet(client, sheet);
+                await fetchSheets();
+              }}
+              className="right-2 top-2 rounded-md bg-red-500 px-2 py-1 text-white transition hover:bg-red-600"
             >
-              <a href={`/sheet/${publisher}/${sheet}`} className="flex-grow">
-                <h2 className="text-xl font-bold">{sheet}</h2>
-                <span>{publisher}</span>
-              </a>
-              <button
-                onClick={async (e) => {
-                  e.preventDefault(); // Prevent default anchor click behavior
-                  await deleteSheet(client, sheet);
-                  await fetchSheets();
-                }}
-                className="right-2 top-2 rounded-md bg-red-500 px-2 py-1 text-white transition hover:bg-red-600"
-              >
-                Delete
-              </button>
-            </div>
-          ))}
-        </div>
+              Delete
+            </button>
+          </div>
+        ))}
       </div>
 
       {isModalOpen && (
