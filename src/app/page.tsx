c5739242625297dc80ad72a7cd2fa7a00968613d
Giastina <morano.gia@northeastern.edu>
2024-06-11 23:53:28 -0400

    login default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 "use client"
 
 //Landing Page
-import Register from "./register/page";
+import Login from "./login/page";
 
 export default function Home() {
   return (
     <div className="mx-auto my-16 flex max-w-sm flex-col gap-4">
-      <Register />
+      <Login />
     </div>
   );
 }
